f70c71ebea3caf9164459bb3a0436f477d6c7a5c
const vag <konsvag@gmail.com>
2022-05-26 17:45:44 +0300

    App configured

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { PlanetsList } from "./src/components/PlanetsList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="cyan" barStyle="dark-content" />
+      <PlanetsList />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#575757",
   },
 });
