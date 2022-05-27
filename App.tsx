f3a394c28189b64f68c036364096e7602a5739fa
const vag <konsvag@gmail.com>
2022-05-27 17:33:14 +0300

    title added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
 import { PlanetsList } from "./src/components/PlanetsList";
+import { Typography } from "./src/ui-kit/Typography";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor="cyan" barStyle="dark-content" />
+      <StatusBar backgroundColor="#f2ebf5" barStyle="dark-content" />
+      <Typography style={styles.title}>Planethopper</Typography>
       <PlanetsList />
     </SafeAreaView>
   );
@@ -14,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#575757",
+    backgroundColor: "#fff",
   },
+  title: {
+    textAlign: "center",
+    fontSize: 50,
+    fontFamily: "monospace",
+  }
 });
