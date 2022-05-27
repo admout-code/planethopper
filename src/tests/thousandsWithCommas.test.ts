65424cce98b3ccf42f480148442f0c1a4722e243
const vag <konsvag@gmail.com>
2022-05-27 19:28:38 +0300

    test case added

--- src/tests/thousandsWithCommas.test.ts
+++ src/tests/thousandsWithCommas.test.ts
@@ -4,6 +4,10 @@ test("10000 to equal 10,000", () => {
   expect(thousandsWithCommas("10000")).toBe("10,000");
 });
 
+test("10000 to not equal 10,000", () => {
+  expect(thousandsWithCommas("10000")).not.toBe("10000");
+});
+
 test("100 to equal 100", () => {
   expect(thousandsWithCommas("100")).toBe("100");
 });
